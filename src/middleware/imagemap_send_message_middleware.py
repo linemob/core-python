0c6bc633e3463b1aef5d28524bad811933c6992f
criticalnooks <mooliveaa@gmail.com>
2017-09-25 23:51:11 +0700

    fix content
    ImageMapSendMessage
    change base_url
    change link_uri

--- src/middleware/imagemap_send_message_middleware.py
+++ src/middleware/imagemap_send_message_middleware.py
@@ -11,12 +11,12 @@ class ImagemapSendMessageMiddleware(Middleware):
 
     def execute(self, command):
         if(command.get_command() == 'command9'):
-            command.set_message({'base_url': 'https://www.google.co.th/',
+            command.set_message({'base_url': 'https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX17605783.jpg',
                                  'alt_text': 'this is an imagemap',
                                  'base_size': BaseSize(height=1040, width=1040),
                                  'actions': [
                                      URIImagemapAction(
-                                         link_uri='https://www.google.com/',
+                                         link_uri='https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX17605783.jpg',
                                          area=ImagemapArea(
                                              x=0, y=0, width=520, height=1040
                                          )

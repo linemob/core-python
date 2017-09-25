60b8e9632475f3592d5348ef1271cd9a6961a3cf
criticalnooks <mooliveaa@gmail.com>
2017-09-25 23:24:13 +0700

    fix bug
    KeyError: 'alt_text'

--- src/middleware/button_template_middleware.py
+++ src/middleware/button_template_middleware.py
@@ -11,22 +11,23 @@ class ButtonTemplateMiddleware(Middleware):
 
     def execute(self, command):
         if(command.get_command() == 'command6'):
-            command.set_message({'thumbnail_image_url': 'https://i.pinimg.com/736x/48/bd/3f/48bd3f6e928d7cb4b8d499cb0f96b8a8--despicable-minions-funny-minion.jpg',
-                                 'title': 'Menu',
-                                 'text': 'Please Select',
-                                 'actions': [
-                                     PostbackTemplateAction(
-                                         label='postback',
-                                         text='postback text',
-                                         data='action=buy&itemid=1'
-                                     ),
-                                     MessageTemplateAction(
-                                         label='message',
-                                         text='message text'
-                                     ),
-                                     URITemplateAction(
-                                         label='uri',
-                                         uri='http://www.google.com/'
-                                     )
-                                 ]
-                                 })
+            command.set_message('alt_text': 'Buttons template',
+                                'template': {'thumbnail_image_url': 'https://i.pinimg.com/736x/48/bd/3f/48bd3f6e928d7cb4b8d499cb0f96b8a8--despicable-minions-funny-minion.jpg',
+                                             'title': 'Menu',
+                                             'text': 'Please Select',
+                                             'actions': [
+                                                 PostbackTemplateAction(
+                                                     label='postback',
+                                                     text='postback text',
+                                                     data='action=buy&itemid=1'
+                                                 ),
+                                                 MessageTemplateAction(
+                                                     label='message',
+                                                     text='message text'
+                                                 ),
+                                                 URITemplateAction(
+                                                     label='uri',
+                                                     uri='http://www.google.com/'
+                                                 )
+                                             ]
+                                             })

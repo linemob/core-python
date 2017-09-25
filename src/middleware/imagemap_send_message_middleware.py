a8653b2bb734c2616d591684198210fe6f41326a
criticalnooks <mooliveaa@gmail.com>
2017-09-25 23:08:27 +0700

    fix bug
    TypeError: execute() takes 1 positional argument but 2 were given

--- src/middleware/imagemap_send_message_middleware.py
+++ src/middleware/imagemap_send_message_middleware.py
@@ -9,7 +9,7 @@ class ImagemapSendMessageMiddleware(Middleware):
     def __init__(self):
         self.do_next = True
 
-    def execute(self):
+    def execute(self, command):
         if(command.get_command() == 'command9'):
             command.set_message({'base_url': 'https://i.pinimg.com/736x/48/bd/3f/48bd3f6e928d7cb4b8d499cb0f96b8a8--despicable-minions-funny-minion.jpg',
                                  'alt_text': 'this is an imagemap',

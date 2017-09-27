7797bcbd9b1e0c3d6f57fa73a8fb96dc0ace12ba
criticalnooks <mooliveaa@gmail.com>
2017-09-27 16:57:04 +0700

    debug
    : TypeError: string indices must be integers

--- src/middleware/default_message_middleware.py
+++ src/middleware/default_message_middleware.py
@@ -5,9 +5,10 @@ class DefaultMessageMiddleware(Middleware):
 
     def __init__(self):
         self.do_next = True
+        self.middleware_command = 'commandDefault'
 
     def execute(self, command):
-        if(command.get_command() == 'commandDefault'):
+        if(command.get_command() == self.middleware_command:
             message = "************\nPress Number for order\n1.TextMessage\n2.ImageMessage\n3.LocationMessage\n4.StickerMessage\n5.ConfirmTemplate\n6.ButtonTemplate\n7.CarouselTemplate\n************"
             command.set_message(message)
             command.set_template('TextMessage')
--- src/middleware/hello_middleware.py
+++ src/middleware/hello_middleware.py
@@ -8,6 +8,8 @@ class HelloMiddleware(Middleware):
         self.middleware_command = '1'
 
     def execute(self, command):
+    	print ('command in hello:'+command.get_command())
         if(command.get_command() == self.middleware_command):
             command.set_message('Hello! World')
             command.set_template('TextMessage')
+            print ('command.get_command() == self.middleware_command')

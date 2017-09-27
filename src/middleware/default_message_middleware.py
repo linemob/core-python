a16fa573f98ea4478bf9a8739aaeae459c504a36
criticalnooks <mooliveaa@gmail.com>
2017-09-27 17:01:11 +0700

    fix bug
    SyntaxError: invalid syntax

--- src/middleware/default_message_middleware.py
+++ src/middleware/default_message_middleware.py
@@ -8,7 +8,7 @@ class DefaultMessageMiddleware(Middleware):
         self.middleware_command = 'commandDefault'
 
     def execute(self, command):
-        if(command.get_command() == self.middleware_command:
+        if(command.get_command() == self.middleware_command):
             message = "************\nPress Number for order\n1.TextMessage\n2.ImageMessage\n3.LocationMessage\n4.StickerMessage\n5.ConfirmTemplate\n6.ButtonTemplate\n7.CarouselTemplate\n************"
             command.set_message(message)
             command.set_template('TextMessage')
